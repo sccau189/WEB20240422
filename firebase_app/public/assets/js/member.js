88b34a4245cc0fa899c69ec904021cb4a266c6d1
sccau189 <sccau189@gmail.com>
2024-07-31 20:42:03 +0800

    用broostrp改寫

--- firebase_app/public/assets/js/member.js
+++ firebase_app/public/assets/js/member.js
@@ -1,7 +1,5 @@
 import { App } from './Firebase/App.js';
 import { Auth } from './Firebase/Auth.js';
-import { Register } from './member/Register.js';
-import { SignIn } from './member/SignIn.js';
 
 let config = {
     apiKey: "AIzaSyD7HJGuVqoFAsPA_lmDsHgmfeWtofgxj70",
@@ -126,33 +124,3 @@ const options = {
 }
 
 Vue.createApp(options).mount('#member-app');
-
-
-
-
-
-// new SignIn(auth);
-// new Register(auth);
-
-// let registerWrap = document.querySelector('#register-wrap');
-// let loginWrap = document.querySelector('#login-wrap');
-// let toLoginBtn = document.querySelector('#to-login-btn');
-// let toRegisterBtn = document.querySelector('#to-register-btn');
-
-
-// toLoginBtn.addEventListener('click', () => {
-//     loginWrap.classList.add('on');
-//     registerWrap.classList.remove('on');
-// })
-
-// toRegisterBtn.addEventListener('click', () => {
-//     loginWrap.classList.remove('on');
-//     registerWrap.classList.add('on');
-// })
-
-// auth.onChange((user) => {
-//     // TODO: 註冊或登入成功時，需先顯示訊息再轉跳
-//     location.href = 'chat.html';
-// }, () => {
-//     loginWrap.classList.add('on');
-// })

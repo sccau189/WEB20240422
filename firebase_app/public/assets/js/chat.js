a24464079c39ad2b04072a438b3316cb7be8fbe4
sccau189 <sccau189@gmail.com>
2024-07-31 21:24:50 +0800

    最後一節課 結束

--- firebase_app/public/assets/js/chat.js
+++ firebase_app/public/assets/js/chat.js
@@ -17,8 +17,66 @@ let app = await App.init(config);
 let database = new Database(app);
 let auth = new Auth(app);
 
-auth.onChange((user) => {
-    new Chat(user, database, auth);
-}, () => {
-    location.href = 'member.html';
-});
+const options = {
+    data() {
+        return {
+            user: {},
+            to: '',
+            users: {
+                'kindping@gmail.com': 'OFDQEDtA6yTpYWVfRcYtx82poGz1',
+                'kindpingwork@gmail.com': 'SZErUk4IebP3swxK59Wfg1MjmOR2'
+            },
+            message: '',
+            messages: {}
+        }
+    },
+    methods: {
+        async doLogout() {
+            await auth.signOut();
+            location.href = 'member.html';
+        },
+        async doSend() {
+            if (!this.to || !this.message) {
+                return;
+            }
+
+            database.insert(`chat/${this.to}`, { from: this.user.email, message: this.message, timer: this.currentTimer() });
+            this.message = '';
+        },
+        currentTimer() {
+            let d = new Date();
+            let hh = d.getHours();
+            let mm = d.getMinutes();
+
+            if (hh < 10) {
+                hh = '0' + hh
+            }
+
+            if (mm < 10) {
+                mm = '0' + mm
+            }
+
+            return `${hh}:${mm}`
+        },
+        doCopyUID() {
+            navigator.clipboard.writeText(this.user.uid);
+            Swal.fire({
+                title: '複製 UID',
+                html: `UID 已複製到剪貼簿`,
+                icon: 'success'
+            })
+        }
+    },
+    mounted() {
+        auth.onChange((user) => {
+            this.user = user
+            database.listen(`chat/${this.user.uid}`, (data) => {
+                this.messages = data;
+            });
+        }, () => {
+            location.href = 'member.html';
+        });
+    }
+}
+
+Vue.createApp(options).mount('#chat-app');
